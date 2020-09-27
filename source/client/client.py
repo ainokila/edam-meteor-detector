90702ccde9b08c718d82f546cfc7dd1e429b3253
ainokila <universidadcvr@gmail.com>
2020-09-27 12:38:48 +0200

    Updates client model

--- source/client/client.py
+++ source/client/client.py
@@ -1,31 +1,61 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys, time, logging, random
+import sys
+import time
+import logging
+import random
 import PyIndi
 
+from PIL import Image
+from io import BytesIO
+from datetime import datetime
+from source.utils.image.formats import fits_to_jpg
 from source.model.indiclients import CCDClient
-  
+
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
- 
+
 server_host = "localhost"
 server_port = 7625
+#server_host = "192.168.0.26"
+#server_port = 7624
 device_name = "QHY CCD QHY5-M-"
 
 # instantiate the client
-ccd_client=CCDClient(device_name=device_name, exposure_time=1)
+ccd_client = CCDClient(device_name=device_name, exposure_time=1)
 
 # set indi server localhost and port
 ccd_client.setServer(server_host, server_port)
 
-
 # connect to indi server
 print("Connecting to indiserver")
 if (not(ccd_client.connectServer())):
-     print("No indiserver running on "+ccd_client.getHost()+":"+str(ccd_client.getPort())+" - Try to run")
-     sys.exit(1)
-  
+    print("No indiserver running on "+ccd_client.getHost() +
+          ":"+str(ccd_client.getPort())+" - Try to run")
+    sys.exit(1)
+
+
+def _save_blob(blobfile, filename, jpg=False):
+
+    with open(filename, "wb") as f:
+        f.write(blobfile.getvalue())
+
+    if jpg:
+        data = fits_to_jpg(filename)
+        # Create image from data array and save as jpg
+        # image = Image.fromarray(data, 'L')
+        # imagename = filename.replace('.fits', '.jpg')
+        # print(image)
+        # image.save(imagename)
+
+
 # start endless loop, client works asynchron in background
 while True:
+
     time.sleep(1)
-    ccd_client.exposure_time = random.randrange(1, 5)
+    ccd_client.exposure_time = 0.1 # random.randrange(1, 5)
+    exposition_time = datetime.now() 
+    ccd_client.takeExposure()
+    fitsfilename = "data/image-%s.fit" % (exposition_time.strftime("%m-%d-%Y-%H:%M:%S"))
+    _save_blob(ccd_client.blob, fitsfilename, jpg=False)
+    ccd_client.logger.info("New image %s", fitsfilename)
--- source/model/indiclients.py
+++ source/model/indiclients.py
@@ -1,101 +1,96 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys, time, logging
+import sys
+import time
+import logging
 import PyIndi
 
-from source.utils.image.formats import fits_to_jpg
 from datetime import datetime
-from PIL import Image
 from io import BytesIO
-  
+import threading
+
+
 class CCDClient(PyIndi.BaseClient):
- 
+
     device = None
-    exposure_time = 1.0
- 
+    logger = logging.getLogger('PyQtIndi.IndiClient')
+
     def __init__(self, device_name, exposure_time=1.0):
         super(CCDClient, self).__init__()
-        self.logger = logging.getLogger('PyQtIndi.IndiClient')
-        self.exposure_time = exposure_time
+        self.blob_event = threading.Event()
         self.device_name = device_name
+        self.exposure_time = exposure_time
+        self.blob = None
 
     def newDevice(self, d):
-        self.logger.info("new device " + d.getDeviceName())
+        self.logger.debug("new device " + d.getDeviceName())
         if d.getDeviceName() == self.device_name:
-            self.logger.info("Set new device %s", self.device_name)
+            self.logger.debug("Set new device %s", self.device_name)
             # save reference to the device in member variable
             self.device = d
 
     def newProperty(self, p):
-        # self.logger.info("new property "+ p.getName() + " for device "+ p.getDeviceName())
+        self.logger.debug("new property "+ p.getName() + " for device "+ p.getDeviceName())
         if self.device is not None and p.getName() == "CONNECTION" and p.getDeviceName() == self.device.getDeviceName():
-            self.logger.info("Got property CONNECTION for CCD")
+            self.logger.debug("Got property CONNECTION for CCD")
             # connect to device
             self.connectDevice(self.device.getDeviceName())
             # set BLOB mode to BLOB_ALSO
             self.setBLOBMode(1, self.device.getDeviceName(), None)
-        if p.getName() == "CCD_EXPOSURE":
-            # take first exposure
-            self.takeExposure()
+
 
     def removeProperty(self, p):
-        self.logger.info("remove property "+ p.getName() + " for device "+ p.getDeviceName())
+        self.logger.info("remove property " + p.getName() +
+                         " for device " + p.getDeviceName())
 
     def newBLOB(self, bp):
         # get image data
         img = bp.getblobdata()
+
         # write image data to BytesIO buffer
-
-        blobfile = BytesIO(img)
-        # open a file and save buffer to disk
-
-        fitsfilename = "data/image-%s.fit" % (datetime.now().strftime("%m-%d-%Y-%H:%M:%S"))
-        with open(fitsfilename, "wb") as f:
-            f.write(blobfile.getvalue())
-
-        self.logger.info("New image %s", fitsfilename)
-
-        # data = fits_to_jpg(fitsfilename)
-
-        # # Create image from data array and save as jpg
-        # image = Image.fromarray(data, 'L')
-        # imagename = fitsfilename.replace('.fits', '.jpg')
-        # image.save(imagename)
-
-        # start new exposure
-        self.takeExposure()
+        self.blob = BytesIO(img)
+        self.blob_event.set()
 
     def newSwitch(self, svp):
-        self.logger.info("new Switch "+ svp.name + " for device "+ svp.device)
+        self.logger.debug("new Switch " + svp.name +
+                         " for device " + svp.device)
 
     def newNumber(self, nvp):
         # self.logger.info("new Number "+ nvp.name + " for device "+ str(nvp.device))
         pass
 
     def newText(self, tvp):
-        self.logger.info("new Text "+ tvp.name + " for device "+ tvp.device)
+        self.logger.debug("new Text " + tvp.name + " for device " + tvp.device)
 
     def newLight(self, lvp):
-        self.logger.info("new Light "+ lvp.name + " for device "+ lvp.device)
+        self.logger.debug("new Light " + lvp.name + " for device " + lvp.device)
 
     def newMessage(self, d, m):
-        #self.logger.info("new Message "+ d.messageQueue(m))
-        pass
+        self.logger.debug("new Message "+ d.messageQueue(m))
 
     def serverConnected(self):
-        print("Server connected ("+self.getHost()+":"+str(self.getPort())+")")
+        self.logger.info("Server connected ("+self.getHost()+":"+str(self.getPort())+")")
 
     def serverDisconnected(self, code):
-        self.logger.info("Server disconnected (exit code = "+str(code)+","+str(self.getHost())+":"+str(self.getPort())+")")
+        self.logger.info("Server disconnected (exit code = "+str(code) +
+                         ","+str(self.getHost())+":"+str(self.getPort())+")")
 
     def takeExposure(self):
 
-        self.logger.info("Request a new exposure using %s seconds", self.exposure_time)
+        self.logger.info(
+            "Request a new exposure using %s seconds", self.exposure_time)
 
-        #get current exposure time
+        # get current exposure time
         exp = self.device.getNumber("CCD_EXPOSURE")
-        # set exposure time
-        exp[0].value = self.exposure_time
-        # send new exposure time to server/device
-        self.sendNewNumber(exp)
+
+        while(not exp):
+            exp = self.device.getNumber("CCD_EXPOSURE")
+
+        if exp:
+            # set exposure time
+            exp[0].value = self.exposure_time
+            # send new exposure time to server/device
+            self.sendNewNumber(exp)
+            self.blob_event.wait()
+            self.blob_event.clear()
