727a66b034d82340b8f6d8f31566be3ae1918bee
ainokila <universidadcvr@gmail.com>
2020-10-05 18:07:27 +0200

    Creates ImageFits model

--- source/model/image/fits.py
+++ source/model/image/fits.py
@@ -10,18 +10,40 @@ class ImageFits(ImageBase):
         ImageBase.__init__(self)
 
     def load_data_from_file(self, path_file):
-        file_data = self._read_file(path_file)
-        self.load_data(file_data)
+        """ Load a fits image from a file
+
+        Args:
+            path_file (str): Image file
+        """
+        self.load_data(self._read_file(path_file))
 
     def load_data(self, file_object):
-        hdu_list = fits.open(file_object)
-        self.data = hdu_list
+        """ Load a fits from a file object
+
+        Args:
+            file_object (FileObject): Python file object
+        """
+        self.data = fits.open(file_object)
+        
 
     def save_image(self, path_file):
-        raise NotImplementedError
+        """ Save the fits in a path
+
+        Args:
+            path_file (str): Path name
+        """
+        self.data.writeto(path_file)
 
     def diff(self, image):
+        """ Compare two fits using ImageDataDiff
+
+        Args:
+            image (ImageFits): Image to compare
+
+        Returns:
+            ImageDataDiff: ImageDataDiff with the differences
+        """
         # a = fits.FITSDiff(self.data, image.data)
         # a.diff_hdus[0][1].diff_data.diff_pixels
-        a = fits.ImageDataDiff(self.data[0], image.data[0])
+        a = fits.ImageDataDiff(self.data[0].data, image.data[0].data)
         return a
