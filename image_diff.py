2fb83d5127e843709d5e77cbe9679a9bae048b6b
ainokila <universidadcvr@gmail.com>
2020-10-10 10:43:10 +0200

    Use opencv

--- image_diff.py
+++ image_diff.py
@@ -13,7 +13,22 @@ b.load_data_from_file('data/image2.fits')
 # a.diff(b).save_image('data/diff.fits')
 # print(diff.data[0].data)
 # 
-cv2.imshow('Viewer', a.diff(b).data[0].data)
+
+# Aplicar suavizado Gaussiano
+img = a.diff(b).data[0].data
+
+gauss = cv2.GaussianBlur(img, (5,5), 0)
+ 
+canny = cv2.Canny(gauss, 50, 150)
+
+# Buscamos los contornos
+(contornos,_) = cv2.findContours(canny.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Mostramos el número de monedas por consola
+print("He encontrado {} objetos".format(len(contornos)))
+
+
+cv2.imshow('Viewer', canny)
 # # print(fits.HDUList(hdus=[fits.ImageHDU(data=backSub.apply(img2))])[0].data)
  
 if cv2.waitKey(20000) & 0xFF == ord('q'):
