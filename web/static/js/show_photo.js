faec6e4a8c714bc28a97b746aa56cc9f8632b4aa
ainokila <universidadcvr@gmail.com>
2020-10-25 12:33:16 +0100

    Creates functionality for image gallery (closes #39)

--- web/static/js/show_photo.js
+++ web/static/js/show_photo.js
@@ -1,34 +1,36 @@
-function image_result(status){
-    //return p1 * p2;   // The function returns the product of p1 and p2
-    console.log("Sending validation for a photo");
+function imageResult(status){
     
     var data = { "photo": document.getElementById("image_view").src, "positive": status};
     var strJSONData = JSON.stringify(data);
+
     $.ajax({
         url: '/result_analyze',
         data: strJSONData,
         dataType : 'json',
         type: "post",
         success: function(response){
-            console.log(response);
-            document.getElementById("image_view").src = response['new_photo'];
+            drawImageInformation(response);
+            composeDownloadUrl();
 
-            document.getElementById("name").innerHTML = response['name'];
-            document.getElementById("header-time").innerHTML = response['header']['time'];
-            document.getElementById("header-exposition").innerHTML = response['header']['exposition'];
-            document.getElementById("header-gain").innerHTML = response['header']['gain'];
-            document.getElementById("header-img_type").innerHTML = response['header']['img_type'];
-            document.getElementById("header-comment").innerHTML = response['header']['comment'];
         },
         error: function(error){
             console.log(error);
         }
     });
-    create_url_for_download()
+}
+
+function drawImageInformation(response){
+    document.getElementById("image_view").src = response['photo'];
+    document.getElementById("name").innerHTML = response['name'];
+    document.getElementById("header-time").innerHTML = response['header']['time'];
+    document.getElementById("header-exposition").innerHTML = response['header']['exposition'];
+    document.getElementById("header-gain").innerHTML = response['header']['gain'];
+    document.getElementById("header-img_type").innerHTML = response['header']['img_type'];
+    document.getElementById("header-comment").innerHTML = response['header']['comment'];
 }
 
 
-function create_url_for_download(){
+function composeDownloadUrl(){
 
     let image = document.getElementById("image_view").src;
     let raw_url = image.replace('jpg', 'fit');
@@ -36,17 +38,78 @@ function create_url_for_download(){
 
 }
 
-window.onload = function() {
-    create_url_for_download();
+function fillGallery(endpoint, from, size){
 
-};
+    var data = { "offset": from, "size": size};
+    var strJSONData = JSON.stringify(data);
+    $.ajax({
+        url: endpoint,
+        data: strJSONData,
+        dataType : 'json',
+        type: "post",
+        success: function(response){
+            var photoPosition;
+            for (photoPosition in response['data']) {
+                var image = response['data'][photoPosition]['photo'];
+                var imageUrl = '/repository/' + image.replace('/static/data/','').replace('.jpg', '');
+                var photoGallery = [
+                    '<div class="col-lg-4 col-md-4 col-6">',
+                            '<img class="img-fluid img-thumbnail showImage" src="' + image +'" alt="">',
+                    '</div>',
+                ];
+                $(photoGallery.join('')).appendTo("#gallery");
+            }
+            console.info(response['data'].length);
+            if (response['data'].length < size){
+                //Disable button btn-load-images
+                $("#btn-load-images").prop("disabled",true);
+            }
+        },
+        error: function(error){
+            console.exception(error);
+            alert('Failed connecting with the server'); 
+        }
+    });
+
+}
 
 
-$("#btn-load-images").click(function() {
-    if (window.location.href.includes("explore")){
-        console.log("Obtener nuevas explore imagenes");
+function nextImages(){
+    var numberPhotos = 3;
+    var endpoint = "";
+    if (window.location.href.includes("positives")){
+        endpoint = '/repository/positives/search';
     }else{
-        console.log("Obtener nuevas validate imagenes");
+        endpoint = '/repository/candidates/search';
     }
-    
+    fillGallery(endpoint, $('#gallery').children().length + 1, numberPhotos);
+    $(document).on('click', '.showImage', showImage);
+}
+
+
+function showImage(){
+    var sourcePhoto = $(this).attr('src');
+    var endPoint = '/repository/' + sourcePhoto.replace('/static/data/', '').replace('.jpg', '');
+    $.ajax({
+        url: endPoint,
+        dataType : 'json',
+        type: "get",
+        success: function(response){
+            drawImageInformation(response);
+            composeDownloadUrl();
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+        },
+        error: function(error){
+            console.log(error);
+        }
+    });
+
+}
+
+
+$( document ).ready(function() {
+    $("#btn-load-images").click(nextImages);
+    composeDownloadUrl();
+    nextImages();
 });
+
