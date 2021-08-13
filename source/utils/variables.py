2fc9e07342f17bcc942f0456a6ad36bdd5466692
ainokila <universidadcvr@gmail.com>
2021-08-13 16:34:06 +0200

    Updates flask version Close #55

--- source/utils/variables.py
+++ source/utils/variables.py
@@ -3,8 +3,7 @@
 
 import os 
 
-
-
 CLIENT_CONFIG_PATH = os.environ.get('CLIENT_CONFIG_PATH', './config/client.json')
 ANALYZER_CONFIG_PATH = os.environ.get('ANALYZER_CONFIG_PATH', './config/analyzer.json')
 REPOSITORY_IMG_DATA_PATH = os.environ.get('REPOSITORY_IMG_DATA_PATH', os.environ['PYTHONPATH'] + '/web/static/data')
+WEB_CONFIG_PATH = os.environ.get('WEB_CONFIG_PATH', os.environ['PYTHONPATH'] + '/config/web.json')
